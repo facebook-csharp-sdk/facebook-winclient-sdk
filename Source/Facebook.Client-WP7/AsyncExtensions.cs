02ed4d975b9c9a97a2fc474ac10ec00468af0665
Nathan Totten <nathan@ntotten.com>
2013-03-26 14:06:33 -0700

    Version to 0.3.0-alpha

--- Source/Facebook.Client-WP7/AsyncExtensions.cs
+++ Source/Facebook.Client-WP7/AsyncExtensions.cs
@@ -9,18 +9,18 @@ namespace Facebook.Client
     internal static class AsyncExtensions
     {
 
-        //public static async Task<object> GetTaskAsync(this FacebookClient client, string path, object parameters)
-        //{
-        //    var tcs = new TaskCompletionSource<object>();
-        //    client.GetCompleted += (s, e) =>
-        //    {
-        //        if (e.Error != null) tcs.TrySetException(e.Error);
-        //        else if (e.Cancelled) tcs.TrySetCanceled();
-        //        else tcs.TrySetResult(e.GetResultData());
-        //    };
-        //    client.GetAsync(path, parameters);
-        //    return tcs.Task;
-        //}
+        public static async Task<object> GetTaskAsync(this FacebookClient client, string path, object parameters)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            client.GetCompleted += (s, e) =>
+            {
+                if (e.Error != null) tcs.TrySetException(e.Error);
+                else if (e.Cancelled) tcs.TrySetCanceled();
+                else tcs.TrySetResult(e.GetResultData());
+            };
+            client.GetAsync(path, parameters);
+            return tcs.Task;
+        }
 
     }
 }
