83867f3845ec9403395eeb60d69be71bd5d36893
ftubio@SW.AD <fernando.tubio@southworks.net>
2013-04-24 11:18:17 -0300

    - Applying styles to control, moved control template from Generic.xaml to resource dictionary specific to the LoginView control, added support to retrieve the user info

---
+++ Source/Facebook.Client-WindowsStore/Controls/Converters/ColorIntensityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Facebook.Client.Controls
+{
+    public class ColorIntensityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
