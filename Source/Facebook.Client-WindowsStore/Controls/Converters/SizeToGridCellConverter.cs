1a91884975e6baffdb5f51a9d7b048e57a7b09b4
ftubio@SW.AD <fernando.tubio@southworks.net>
2013-05-10 02:53:01 -0300

    Added XML code comments to SizeToGridCellConverter.

--- Source/Facebook.Client-WindowsStore/Controls/Converters/SizeToGridCellConverter.cs
+++ Source/Facebook.Client-WindowsStore/Controls/Converters/SizeToGridCellConverter.cs
@@ -6,8 +6,19 @@
     using Windows.UI.Xaml.Controls;
     using Windows.UI.Xaml.Data;
 
+    /// <summary>
+    /// Calculates the size of group templates used by the zoomed out view to arrange them evenly.
+    /// </summary>
     public class SizeToGridCellConverter : IValueConverter
     {
+        /// <summary>
+        /// Calculates the width and height of the group templates based on the size of the control.
+        /// </summary>
+        /// <param name="value">A reference to the control hosting the group templates.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">Unused.</param>
+        /// <param name="language">The culture to use in the converter (unused).</param>
+        /// <returns>.</returns>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var control = (Control)value;
@@ -30,6 +41,9 @@
             }
         }
 
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
