94c3b6f1e71755d3e4f5021f33096a0884ad8b33
ftubio@SW.AD <fernando.tubio@southworks.net>
2013-05-29 00:50:28 -0300

    Updated sample app for Windows 8 to produce similar results to the one shown in the tutorials

---
+++ Samples/BasicApp-WindowsStore/EnumConverter.cs
@@ -0,0 +1,30 @@
+﻿namespace BasicApp
+{
+    using System;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Data;
+
+    public class CropModeToBooleanConverter : EnumToBooleanConverter<Facebook.Client.Controls.CropMode>
+    {
+    }
+
+    public class EnumToBooleanConverter<T> : IValueConverter
+        where T : struct
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Type enumType = typeof(T);
+            string s = Enum.GetName(enumType, value);
+            object b = Enum.Parse(enumType, parameter.ToString());
+            return b.ToString() == s;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {         
+            Type enumType = typeof(T);
+            return Enum.Parse(enumType, parameter.ToString());
+        }
+    }
+}
+
--- Samples/BasicApp-WindowsStore/MainPage.xaml.cs
+++ Samples/BasicApp-WindowsStore/MainPage.xaml.cs
@@ -16,6 +16,7 @@
         public MainPage()
         {
             this.InitializeComponent();
+            this.currentItem = this.welcomeText;
             ////this.Loaded += MainPage_Loaded;
         }
 
@@ -53,6 +54,31 @@
         {
         }
 
+        private UIElement currentItem;
+
+        private void OnItemSelected(object sender, RoutedEventArgs e)
+        {
+            var choice = sender as RadioButton;
+            if (choice != null)
+            {
+                this.currentItem.Visibility = Visibility.Collapsed;
+                switch (choice.Name)
+                {
+                    case "user":
+                        this.currentItem = this.userInfo; break;
+                    case "picture":
+                        this.currentItem = this.profilePictureScenario; break;
+                    case "friends":
+                        this.currentItem = this.friendPickerScenario; break;
+                    case "places":
+                        this.currentItem = this.placePickerScenario; break;
+                }
+
+                this.currentItem.Visibility = Visibility.Visible;
+                this.currentItem.Opacity = 100;
+            }
+        }
+
         private void OnUserInfoChanged(object sender, Facebook.Client.Controls.UserInfoChangedEventArgs e)
         {
             this.userInfo.DataContext = e.User;
@@ -61,7 +87,11 @@
         private void OnSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
         {
             this.welcomeText.Opacity = (e.SessionState == FacebookSessionState.Opened) ? 0 : 100;
-            this.contentPanel.Opacity = (e.SessionState == FacebookSessionState.Opened) ? 100 : 0; 
+            this.ContentPanel.Opacity = (e.SessionState == FacebookSessionState.Opened) ? 100 : 0;
+            this.userInfo.Visibility = Visibility.Collapsed;
+            this.placePickerScenario.Visibility = Visibility.Collapsed;
+            this.friendPickerScenario.Visibility = Visibility.Collapsed;
+            this.placePickerScenario.Visibility = Visibility.Collapsed;
         }
 
         private void OnFriendRetrieved(object sender, DataItemRetrievedEventArgs<Facebook.Client.GraphUser> e)
@@ -76,6 +106,16 @@
         {
             var msgbox = new MessageDialog(e.Reason, e.Description);
             await msgbox.ShowAsync();
+        }
+
+        private void OnDisplayOrderSelected(object sender, RoutedEventArgs e)
+        {
+            if (this.friendPicker != null)
+            {
+                var choice = sender as RadioButton;
+                this.friendPicker.DisplayOrder =
+                    (Facebook.Client.Controls.FriendPickerDisplayOrder)Enum.Parse(typeof(Facebook.Client.Controls.FriendPickerDisplayOrder), choice.Name);
+            }
         }
     }
 }
