1cc3a7c959e7ff26cfba22fc2e082bdf72d4c74e
Sanjeev Dwivedi <sanjeev.dwivedi@gmail.com>
2015-01-20 23:35:31 -0800

    Adding a non-optimal dialog in Windows branch

--- Samples/BasicApp-Win81-Universal/MainPage.xaml.cs
+++ Samples/BasicApp-Win81-Universal/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,6 +15,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using Facebook.Client;
 
 namespace BasicApp_Win81_Universal
 {
@@ -25,6 +27,25 @@ namespace BasicApp_Win81_Universal
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.dialogWebView.NavigationStarting += dialogWebView_NavigationStarting;
+
+        }
+
+        void dialogWebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            if (args.Uri.ToString().StartsWith("https://pratapgarh.com"))
+            {
+                dialogWebView.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        
+
+        private void showDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.dialogWebView.Visibility = Visibility.Visible;
+            this.dialogWebView.Navigate(new Uri("https://facebook.com/dialog/apprequests?display=popup&app_id=540541885996234&message=YOUR_MESSAGE_HERE!&redirect_uri=https://pratapgarh.com&access_token=" + Session.ActiveSession.CurrentAccessTokenData.AccessToken, UriKind.Absolute));
         }
     }
 }
