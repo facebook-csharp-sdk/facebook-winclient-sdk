0d2012e9a59412a51713b52cc8e7a4189f7f0caa
Sanjeev Dwivedi <sanjeev.dwivedi@gmail.com>
2015-02-26 23:13:07 -0800

    release a new nuget and bumping up the version to 1.0.2

--- Source/Facebook.Client/Properties/AssemblyInfo.cs
+++ Source/Facebook.Client/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) 2011, The Outercurve Foundation.")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.2")]
